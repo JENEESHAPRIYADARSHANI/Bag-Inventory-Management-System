0922976ec6b32ba3a1f6002ef05e990edb6513a8
palaram29 <palaram829@gmail.com>
2026-01-31 01:23:12 +0530

    Creating Supplier-Material-Management-Service

---
+++ Backend/Supplier-Material-Management-Service/src/test/java/com/starbag/Supplier_Material_Management_Service/SupplierMaterialManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbag.Supplier_Material_Management_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierMaterialManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
