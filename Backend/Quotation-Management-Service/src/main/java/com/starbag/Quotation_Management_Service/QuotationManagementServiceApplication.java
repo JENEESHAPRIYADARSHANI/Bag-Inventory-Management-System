f93a1c3c8d046d25431824a7a5cd5428f119e939
sajeevanh2r <sajeevan2001625@gmail.com>
2026-02-02 00:16:53 +0530

    creating Quotation Management Service

---
+++ Backend/Quotation-Management-Service/src/main/java/com/starbag/Quotation_Management_Service/QuotationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.starbag.Quotation_Management_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationManagementServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/Quotation-Management-Service/src/test/java/com/starbag/Quotation_Management_Service/QuotationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbag.Quotation_Management_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotationManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
