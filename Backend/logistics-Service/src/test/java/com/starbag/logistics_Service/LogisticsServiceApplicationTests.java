3fcbeed0b8682a7e7dfa6a3cb207e804aacbf4bc
Nuskyn <nusky043@gmail.com>
2026-02-03 01:29:45 +0530

    Initial commit for Logistics Service

---
+++ Backend/logistics-Service/src/test/java/com/starbag/logistics_Service/LogisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starbag.logistics_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
