0ccedaf85a22de820087478fbf1583cd12cb775c
palaram29 <palaram829@gmail.com>
2026-02-18 23:33:04 +0530

    Created Material class with fields for name, type, unit, unit price, reorder level, and status (ACTIVE or DISABLED).

---
+++ Backend/Supplier-Material-Management-Service/src/main/java/com/starbag/Supplier_Material_Management_Service/domain/Material.java
@@ -0,0 +1,36 @@
+package com.starbag.Supplier_Material_Management_Service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.net.Inet4Address;
+import java.util.Stack;
+
+@Entity
+@Table(name ="")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Material {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private String unit;
+    private Double unitPrice;
+    private Integer reorderLevel;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status{
+        ACTIVE, DISABLED
+    }
+
+}
