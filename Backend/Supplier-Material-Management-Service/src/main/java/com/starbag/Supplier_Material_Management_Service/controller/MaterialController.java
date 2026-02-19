f88923a8674b7e18682a8609ca89b6ed294bc659
palaram29 <palaram829@gmail.com>
2026-02-19 12:40:31 +0530

    Add create, update, and delete endpoints for materials

---
+++ Backend/Supplier-Material-Management-Service/src/main/java/com/starbag/Supplier_Material_Management_Service/controller/MaterialController.java
@@ -0,0 +1,41 @@
+package com.starbag.Supplier_Material_Management_Service.controller;
+
+
+import com.starbag.Supplier_Material_Management_Service.dto.MaterialDto;
+import com.starbag.Supplier_Material_Management_Service.service.MaterialService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/materials")
+@RequiredArgsConstructor
+public class MaterialController {
+    private final MaterialService service;
+
+    @GetMapping
+    public List<MaterialDto> getAll() {
+        return service.getAllMaterials();
+    }
+
+    @GetMapping("/{id}")
+    public MaterialDto getById(@PathVariable Long id){
+        return service.getMaterialById(id);
+    }
+
+    @PostMapping
+    public MaterialDto create(@RequestBody MaterialDto dto){
+        return service.createMaterial(dto);
+    }
+    @PutMapping("/{id}")
+    public MaterialDto update(@PathVariable Long id, @org.springframework.web.bind.annotation.RequestBody MaterialDto dto){
+        return service.updateMaterial(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        service.deleteMaterial(id);
+    }
+}
