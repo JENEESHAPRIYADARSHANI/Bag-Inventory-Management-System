2171b2596b6b8e203f6e1c709e7bafab38dac189
palaram29 <palaram829@gmail.com>
2026-02-19 16:56:55 +0530

    Add supplyPrice and leadTimeDays fields

--- Backend/Supplier-Material-Management-Service/src/main/java/com/starbag/Supplier_Material_Management_Service/domain/SupplierMaterialMapping.java
+++ Backend/Supplier-Material-Management-Service/src/main/java/com/starbag/Supplier_Material_Management_Service/domain/SupplierMaterialMapping.java
@@ -1,4 +1,31 @@
 package com.starbag.Supplier_Material_Management_Service.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "supplier_materials")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SupplierMaterialMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+    @ManyToOne
+    @JoinColumn(name = "material_id")
+    private Material material;
+
+    private Double supplyPrice;
+    private Integer leadTimeDays;
 }
