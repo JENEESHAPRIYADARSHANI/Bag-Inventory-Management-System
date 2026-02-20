3ca104e33b32930387821c6d61abe8cf4ccebf1c
palaram29 <palaram829@gmail.com>
2026-02-20 23:25:25 +0530

    Initializing 3rd CRUD operation Implement createMapping() and updateMapping() methods

--- Backend/Supplier-Material-Management-Service/src/main/java/com/starbag/Supplier_Material_Management_Service/service/impl/SupplierMaterialMappingServiceImpl.java
+++ Backend/Supplier-Material-Management-Service/src/main/java/com/starbag/Supplier_Material_Management_Service/service/impl/SupplierMaterialMappingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.starbag.Supplier_Material_Management_Service.service.impl;
 
-
+import com.starbag.Supplier_Material_Management_Service.domain.Supplier;
+import com.starbag.Supplier_Material_Management_Service.domain.Material;
+import com.starbag.Supplier_Material_Management_Service.domain.SupplierMaterialMapping;
 import com.starbag.Supplier_Material_Management_Service.dto.SupplierMaterialMappingDto;
 import com.starbag.Supplier_Material_Management_Service.exception.ResourceNotFoundException;
 import com.starbag.Supplier_Material_Management_Service.repository.MaterialRepository;
@@ -28,6 +30,36 @@ public class SupplierMaterialMappingServiceImpl implements SupplierMaterialMappi
     public SupplierMaterialMappingDto getMappingById(Long id){
         return repository.findById(id).map(this::toDto)
                 .orElseThrow(() -> new ResourceNotFoundException("Mapping not found"));
+    }
+
+    @Override
+    public SupplierMaterialMappingDto createMapping(SupplierMaterialMappingDto dto){
+        Supplier supplier = supplierRepository.findById((dto.getSupplierId()))
+                .orElseThrow(() -> new ResourceNotFoundException("Supplier not found"));
+        Material material = materialRepository.findById((dto.getMaterialId()))
+                .orElseThrow(()-> new ResourceNotFoundException("Material not found"));
+        SupplierMaterialMapping mapping = SupplierMaterialMapping.builder()
+                .supplier(supplier)
+                .material(material)
+                .supplyPrice(dto.getSupplyPrice())
+                .leadTimeDays(dto.getLeadTimeDays())
+                .build();
+        return toDto(repository.save(mapping));
+    }
+
+    @Override
+    public SupplierMaterialMappingDto updateMapping(Long id, SupplierMaterialMappingDto dto){
+        SupplierMaterialMapping mapping = repository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Mapping not found"));
+        Supplier supplier = supplierRepository.findById(dto.getSupplierId())
+                .orElseThrow(()-> new ResourceNotFoundException("Supplier not found"));
+        Material material = materialRepository.findById(dto.getMaterialId())
+                .orElseThrow(()-> new ResourceNotFoundException("Material not found"));
+        mapping.setSupplier(supplier);
+        mapping.setMaterial(material);
+        mapping.setSupplyPrice(dto.getSupplyPrice());
+        mapping.setLeadTimeDays(dto.getLeadTimeDays());
+        return toDto(repository.save(mapping));
     }
 
 }
