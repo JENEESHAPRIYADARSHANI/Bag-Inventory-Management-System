98e15fc593c750ef1dabb7116955c572f4d313c7
PDhanusha19 <168332466+PDhanusha19@users.noreply.github.com>
2026-03-07 01:03:57 +0530

    Update Product.java

--- Backend/product-catalog-service/src/main/java/com/starbag/product_catalog_service/domain/Product.java
+++ Backend/product-catalog-service/src/main/java/com/starbag/product_catalog_service/domain/Product.java
@@ -10,16 +10,21 @@ import java.util.List;
 public class Product {
 
     @Id
-    private String id; // e.g., "PRD-001"
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
+
     private String category;
+
     private String price;
+
     private String material;
+
     private String dimensions;
+
     private String capacity;
 
-    // One Product has Many Variants. CascadeType.ALL means if we save/delete a product, it saves/deletes its variants too.
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "product_id")
     private List<ProductVariant> variants;
