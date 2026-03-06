daaba16317f6f3ac6181a0f138cd65eaf28a762a
PDhanusha19 <168332466+PDhanusha19@users.noreply.github.com>
2026-03-07 01:08:09 +0530

    Update ProductRepository.java

--- Backend/product-catalog-service/src/main/java/com/starbag/product_catalog_service/repository/ProductRepository.java
+++ Backend/product-catalog-service/src/main/java/com/starbag/product_catalog_service/repository/ProductRepository.java
@@ -4,6 +4,6 @@ import com.starbag.product_catalog_service.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
